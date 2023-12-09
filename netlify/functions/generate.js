8989b7bcd000cada53bd5dc8ccc606498f346248
tahaakgungor <tahaakgungor@gmail.com>
2023-12-09 21:20:47 +0300

    updated

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -1,23 +1,32 @@
-// netlify/functions/generate.js
 export async function handler(event, context) {
-    // Burada form verilerini alabilir ve Flask uygulamanızın API'sine yönlendirebilirsiniz.
-    // Örnek bir işlem:
-    const requestBody = JSON.parse(event.body);
+    try {
+        // Gelen veriyi JSON formatında parse et
+        const requestBody = JSON.parse(event.body);
+        console.log('Parsed request body:', requestBody);
 
-    // Flask uygulamanızın API'sine verileri POST isteği ile gönderme örneği
-    const response = await fetch('https://creativead.netlify.app/generate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-    });
+        // Flask uygulamanızın API'sine verileri POST isteği ile gönderme örneği
+        const response = await fetch('https://creativead.netlify.app/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody),
+        });
 
-    const responseData = await response.json();
+        // Flask uygulamasından gelen cevabı JSON formatında al
+        const responseData = await response.json();
 
-    // Netlify'e geri dönecek cevap
-    return {
-        statusCode: response.status,
-        body: JSON.stringify(responseData),
-    };
+        // Netlify'e geri dönecek cevap
+        return {
+            statusCode: response.status,
+            body: JSON.stringify(responseData),
+        };
+    } catch (error) {
+        console.error('Error processing request:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
 }
