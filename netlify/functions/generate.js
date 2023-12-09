c8fc47cf1c36f911223f614eee41871b9afc8e42
tahaakgungor <tahaakgungor@gmail.com>
2023-12-09 21:33:24 +0300

    added_form_data

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -1,20 +1,34 @@
+import FormData from 'form-data';
+
 export async function handler(event, context) {
     try {
-        // Gelen veriyi JSON formatında parse et
-        const requestBody = JSON.parse(event.body);
-        console.log('Parsed request body:', requestBody);
+        const formData = new FormData();
 
-        // Flask uygulamanızın API'sine verileri POST isteği ile gönderme örneği
-        const response = await fetch('https://creativead.netlify.app/generate', {
+        // Gelen verileri JSON formatında parse et
+        const requestBody = JSON.parse(event.body);
+
+        console.log('Request body:', requestBody);
+
+        // Form alanlarını FormData nesnesine ekleyin
+        formData.append('image', requestBody.image); // Örnek: 'image' form alanı
+        formData.append('prompt', requestBody.prompt);
+        formData.append('color', requestBody.color);
+        formData.append('logo', requestBody.logo);
+        formData.append('punchline', requestBody.punchline);
+        formData.append('punchline_color', requestBody.punchline_color);
+        formData.append('button_text', requestBody.button_text);
+        formData.append('button_color', requestBody.button_color);
+
+        // Flask uygulamanızın API'sine verileri POST isteği ile gönderme
+        const response = await fetch('https://creativead.netlify.app/.netlify/functions/generate', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(requestBody),
+            body: formData,
         });
 
         // Flask uygulamasından gelen cevabı JSON formatında al
         const responseData = await response.json();
+
+        console.log('Response from API:', responseData);
 
         // Netlify'e geri dönecek cevap
         return {
